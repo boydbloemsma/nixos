d8a098a24731030d8aa1c69a5ad9a162927a45fe
Boyd Bloemsma <boydbloemsma@gmail.com>
2025-04-12 19:26:29 +0200

    feat: added lf

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -107,6 +107,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    lf
     wget
     git
     neovim
