5cd3e11c412cf4a8544f4b4a4318b3d35f90b9f8
Boyd Bloemsma <boydbloemsma@gmail.com>
2025-04-12 19:16:39 +0200

    feat: added alias to get nvim from github

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -1,23 +1,23 @@
 { config, lib, ... }: {
 
-  options = {
-    my.zsh.enable = lib.mkEnableOption "Enables zsh";
-  };
-
-  config = lib.mkIf config.my.zsh.enable {
-    programs.zsh = {
-      enable = true;
-
-      shellAliases = {
-        lg = "lazygit";
-      };
-
-      oh-my-zsh = {
-        enable = true;
-        theme = "robbyrussell";
-        plugins = [ "git" ];
-      };
+    options = {
+        my.zsh.enable = lib.mkEnableOption "Enables zsh";
     };
-  };
 
+    config = lib.mkIf config.my.zsh.enable {
+        programs.zsh = {
+            enable = true;
+
+            shellAliases = {
+                lg = "lazygit";
+                nvim = "nix run github:boydbloemsma/neovim-flake";
+            };
+
+            oh-my-zsh = {
+                enable = true;
+                theme = "robbyrussell";
+                plugins = [ "git" ];
+            };
+        };
+    };
 }
