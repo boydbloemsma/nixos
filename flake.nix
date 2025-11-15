909375dad084efe81113e37992c27586f8a5c810
Boyd Bloemsma <boydbloemsma@gmail.com>
2025-11-15 18:48:56 +0100

    feat: added temp kamal overwrite

--- flake.nix
+++ flake.nix
@@ -31,5 +31,30 @@
     };
 
     homeManagerModules.default = ./modules/home-manager;
+
+    # Temporary nix develop shell because kamal is not up to date in nix packages
+    # When it is up to date, remove the .kamal-gems dir and this code
+    devShells.x86_64-linux.kamal = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [
+        ruby
+        bundler
+        gcc
+        gnumake
+      ];
+
+      shellHook = ''
+          export GEM_HOME="$HOME/.kamal-gems"
+          export GEM_PATH="$GEM_HOME"
+          export PATH="$GEM_HOME/bin:$PATH"
+          unset GEM_ROOT
+
+          if [ ! -f .kamal-gems/bin/kamal ]; then
+            echo "Installing Kamal 2.8.2..."
+            gem install kamal -v 2.8.2
+          fi
+      '';
+    };
   };
 }
