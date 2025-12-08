79242702dc33997e61177588cfff18f0ef88fcb1
Boyd Bloemsma <boydbloemsma@gmail.com>
2025-12-08 20:55:50 +0100

    feat: added accent color and wallpaper

--- modules/home-manager/gnome-settings.nix
+++ modules/home-manager/gnome-settings.nix
@@ -5,6 +5,13 @@
     dconf.settings = {
       "org/gnome/desktop/interface" = {
         enable-hot-corners = false;
+        accent-color = "blue";
+      };
+
+      "org/gnome/desktop/background" = {
+        picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/lcd-rainbow-l.jxl";
+        picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/lcd-rainbow-d.jxl";
+        picture-options = "zoom";
       };
 
       "org/gnome/desktop/wm/keybindings" = {
