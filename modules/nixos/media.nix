964ef3db224ac4cf5ee33ec5ec15a84c204d027d
Boyd Bloemsma <boydbloemsma@gmail.com>
2026-01-22 13:57:36 +0100

    chore: replaced plex with jellyfin

--- modules/nixos/media.nix
+++ modules/nixos/media.nix
@@ -1,5 +1,11 @@
-{ config, lib, pkgs, ... }: {
-  options.media.enable = lib.mkEnableOption "Install VLC, Plex, qbittorrent and Proton VPN";
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.media.enable = lib.mkEnableOption "Install VLC, Jellyfin, qbittorrent and Proton VPN";
 
   config = lib.mkIf config.media.enable {
     environment.systemPackages = with pkgs; [
@@ -7,13 +13,15 @@
       qbittorrent
       protonvpn-gui
       rawtherapee
+      jellyfin
+      jellyfin-web
+      jellyfin-ffmpeg
     ];
 
-        # services.plex = {
-        # enable = true;
-        # openFirewall = true;
-        # user = "boyd";
-        # group = "users";
-        # };
+    services.jellyfin = {
+      enable = true;
+      openFirewall = true;
+      user = "boyd";
+    };
   };
 }
