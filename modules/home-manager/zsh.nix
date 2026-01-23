4140b2dcbebb73f9b0b8704419475a36a8f0fa37
Boyd Bloemsma <boydbloemsma@gmail.com>
2026-01-23 22:55:59 +0100

    feat: changed default zsh theme to lambda

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -1,24 +1,25 @@
-{ config, lib, ... }: {
+{ config, lib, ... }:
+{
 
-    options = {
-      zsh.enable = lib.mkEnableOption "Enables zsh";
+  options = {
+    zsh.enable = lib.mkEnableOption "Enables zsh";
+  };
+
+  config = lib.mkIf config.zsh.enable {
+    programs.zsh = {
+      enable = true;
+
+      shellAliases = {
+        lg = "lazygit";
+        ld = "lazydocker";
+        nvim = "nix run github:boydbloemsma/neovim-flake";
+      };
+
+      oh-my-zsh = {
+        enable = true;
+        theme = "lambda";
+        plugins = [ "git" ];
+      };
     };
-
-    config = lib.mkIf config.zsh.enable {
-        programs.zsh = {
-            enable = true;
-
-            shellAliases = {
-                lg = "lazygit";
-                ld = "lazydocker";
-                nvim = "nix run github:boydbloemsma/neovim-flake";
-            };
-
-            oh-my-zsh = {
-                enable = true;
-                theme = "robbyrussell";
-                plugins = [ "git" ];
-            };
-        };
-    };
+  };
 }
