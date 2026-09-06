fdecc459a53aecb47c94e4c6c9a039ec5db69c05
Boyd Bloemsma <boydbloemsma@gmail.com>
2026-09-06 22:39:27 +0200

    feat: added prodesk as ssh target

--- hosts/t480/home.nix
+++ hosts/t480/home.nix
@@ -83,6 +83,30 @@
     };
   };
 
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    settings = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+      prodesk = {
+        hostname = "prodesk";
+        user = "boydbloemsma";
+        forwardAgent = true;
+      };
+    };
+  };
+
   ghostty.enable = true;
   code-dir.enable = true;
   zed.enable = true;
